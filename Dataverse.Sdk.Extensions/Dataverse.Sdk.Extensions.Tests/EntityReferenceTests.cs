6be4bbd5e052c3ece07dd4c091a26c9b2fbbc7af
Daryl LaBar <daryl@gapintegrity.com>
2021-05-04 21:36:05 -0400

    Added ToStringDebug and EntityReference.GetNameId extensions

---
+++ Dataverse.Sdk.Extensions/Dataverse.Sdk.Extensions.Tests/EntityReferenceTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Xrm.Sdk;
+using System;
+using Xunit;
+
+namespace Dataverse.Sdk.Extensions.Tests
+{
+    public class EntityReferenceTests
+    {
+        [Fact]
+        public void GetNameId_Can_Display_Name_And_Id()
+        {
+            // Arrange
+            var nullValue = (EntityReference) null;
+            var empty = new EntityReference();
+            var idOnly = new EntityReference("account", Guid.NewGuid());
+            var full = new EntityReference("account", Guid.NewGuid())
+            {
+                Name = "Bart Simpson"
+            };
+
+            //Assert 
+            Assert.Equal("null", nullValue.GetNameId());
+            Assert.Equal(Guid.Empty.ToString(), empty.GetNameId());
+            Assert.Equal(idOnly.Id.ToString(), idOnly.GetNameId());
+            Assert.Equal($"{full.Name} ({full.Id})", full.GetNameId());
+            Assert.Equal($"{full.Name} {full.Id}", full.GetNameId("{0} {1}"));
+        }
+    }
+}
---
+++ Dataverse.Sdk.Extensions/Dataverse.Sdk.Extensions/EntityReferenceExtensions.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Xrm.Sdk;
+
+namespace Dataverse.Sdk.Extensions
+{
+    public static class EntityReferenceExtensions
+    {
+        /// <summary>
+        /// Returns the Name and Id of an entity reference in a format "Name (id)" (default) if it has a name, and "(id)" if it has no name.
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <param name="formatNonEmptyName">The format to use when displaying the name and id.  {0} is the name of the entity reference, {1} is the Guid and {2} is the logical name.</param>
+        /// <param name="formatEmptyName">The format to use when displaying the name id.  {0} is the name of the entity reference, {1} is the Guid and {2} is the logical name.</param>
+        /// <param name="nullFormat">The value to use when the entity reference is null..</param>
+        /// <returns></returns>
+        public static string GetNameId(this EntityReference entity, string formatNonEmptyName = "{0} ({1})", string formatEmptyName = "{1}", string nullFormat = "null")
+        {
+            if (entity == null)
+            {
+                return nullFormat;
+            }
+
+            return string.IsNullOrWhiteSpace(entity.Name)
+                ? string.Format(formatEmptyName, entity.Name, entity.Id, entity.LogicalName)
+                : string.Format(formatNonEmptyName, entity.Name, entity.Id, entity.LogicalName);
+        }
+    }
+}
